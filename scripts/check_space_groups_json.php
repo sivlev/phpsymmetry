7349bda1100370aaede1bc7618cd96b550b130a1
Sergei Ivlev <sergei.ivlev@chemie.uni-marburg.de>
2024-05-17 21:06:05 +0200

    Replace a translation with b translation in Hall symbols of groups 64 and 67 to match the symmetry operators generated by Hall and other methods

---
+++ scripts/check_space_groups_json.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPSymmetry\SymmetryGroup\SpaceGroup;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+echo 'This script will perform the integrity check of the spacegroups.json file.' . PHP_EOL;
+echo PHP_EOL;
+
+$f = file_get_contents(__DIR__ . '/../data/spacegroups.json');
+if ($f === false) {
+    echo 'Error: Cannot read the spacegroups.json file.' . PHP_EOL;
+    exit(1);
+}
+
+$spacegroups = json_decode($f, true);
+if ($spacegroups === null) {
+    echo 'Error: Cannot decode the spacegroups.json file.' . PHP_EOL;
+    exit(1);
+}
+
+foreach ($spacegroups as $s) {
+    echo 'Checking space group ' . $s['number'] . '... ' . PHP_EOL;
+    echo 'Generating the space group from the Hall symbol: ' . $s['hall'] . '... ';
+    $sHall = SpaceGroup::makeFromHallSymbol($s['hall'])->expandGroup();
+    echo 'done.' . PHP_EOL;
+    //foreach ($sHall->symmetryOperations() as $i => $op) {
+    //    echo '  ' . ($i + 1) . '. ' . $op->toSymbol() . PHP_EOL;
+    //}
+
+    echo 'Generating the space group from the explicit symbol: ' . $s['explicit'] . '... ';
+    $sExplicit = SpaceGroup::makeFromExplicitSymbol($s['explicit'])->expandGroup();
+    echo 'done.' . PHP_EOL;
+    //echo $sExplicit;
+
+    echo 'Generating the space group from the generators: ' . $s['generators'] . '... ';
+    $sGenerators = SpaceGroup::makeFromExplicitSymbol($s['generators'])->expandGroup();
+    echo 'done.' . PHP_EOL;
+
+    echo 'Comparing the space group operations from the Hall symbol and from the explicit symbol... ';
+    if (!$sHall->isEqual($sExplicit)) {
+        echo 'Error: The space group operations do not match.' . PHP_EOL;
+        exit(1);
+    }
+    echo 'done.' . PHP_EOL;
+
+    echo 'Comparing the space group operations from the Hall symbol and from the generators... ';
+    if (!$sHall->isEqual($sGenerators)) {
+        echo 'Error: The space group operations do not match.' . PHP_EOL;
+        exit(1);
+    }
+    echo 'done.' . PHP_EOL;
+
+    echo 'Comparing the space group operations from the explicit symbol and from the generators... ';
+    if (!$sExplicit->isEqual($sGenerators)) {
+        echo 'Error: The space group operations do not match.' . PHP_EOL;
+        exit(1);
+    }
+    echo 'done.' . PHP_EOL;
+
+    echo PHP_EOL;
+}
